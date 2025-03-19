14ed3182bf7b35407f2c2d62080fcf7238bf885e
EbonyIvory0 <pechka959@gmail.com>
2025-03-19 13:08:49 +0300

    update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,21 +1,32 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import tempfile
+import shutil
+import uuid
 
 @pytest.fixture(scope="session")
 def browser():
-    # Настройка опций Chrome
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Запуск в headless-режиме
-    chrome_options.add_argument("--disable-gpu")  # Отключение GPU (для headless)
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=34221")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    user_data_dir = tempfile.mkdtemp(prefix=f"chrome_{uuid.uuid1()}_")
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    service = Service(executable_path="/snap/bin/chromedriver") # указать свой путь к драйверу
+    driver = webdriver.Chrome(service=service, options=options)
 
     driver.get("https://app.staging.sphera.work/")
     driver.maximize_window()
 
+    # Выполните авторизацию здесь, если это необходимо
     yield driver
+
     driver.quit()
+    shutil.rmtree(user_data_dir)
