f521367881a71c042cfd7a6641d9342535ed87fa
EbonyIvory0 <pechka959@gmail.com>
2025-04-18 16:56:52 +0300

    start negative test auth

---
+++ tests/test_negative_auth.py
@@ -0,0 +1,26 @@
+import pytest
+from faker import Faker
+from utils.logger import Logger
+from pages.login_page import AuthorizationPage
+
+fake = Faker()
+logger = Logger()
+
+
+invalid_emails = [
+    f"@{fake.domain_name()}",              # Отсутствует локальная часть
+    f"{fake.user_name()}@",                # Нет домена
+    f"{fake.user_name()}@{fake.word()}",   # Домен без точки
+    f"{'a'*256}@example.com",              # Слишком длинный email
+    "me@.com",                             # Домен начинается с точки
+    "john.doe@-example.com",               # Домен начинается с дефиса
+    "тест@example.com",                    # Кириллица в локальной части
+    "user@example..com",                   # Две точки подряд в домене
+    "user@example_com",                    # Подчеркивание в домене
+    "user@.com"                            # Пустой домен
+]
+
+@pytest.mark.qwe
+def test_12_negative_auth(browser):
+    auth_page = AuthorizationPage(browser)
+    auth_page.test_invalid_emails(invalid_emails)
